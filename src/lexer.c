25a1578b4e039d9f7cca5fa35ee388de2044cce1
AW-AlanWu <alan.92925@gmail.com>
2024-11-16 20:28:51 +0800

    Support reallocate token

--- src/lexer.c
+++ src/lexer.c
@@ -36,6 +36,23 @@ token_t *alloc_token(token_arena_t *arena, int size)
     return data;
 }
 
+token_t *realloc_token(token_arena_t *arena, token_t *tail, int size)
+{
+    /* TODO: Expand arena if remaining capacity is not enough */
+    if (arena->size + size > arena->capacity)
+        return NULL;
+
+    token_t *data = &tail[1], *cur = data;
+    for (int i = 1; i < size; i++) {
+        cur->next = &data[i];
+        cur = cur->next;
+    }
+    tail->next = data;
+    cur->next = NULL;
+    arena->size += size;
+    return cur;
+}
+
 /* Copies single token data to a new token */
 token_t *arena_copy_token(token_arena_t *arena, token_t *token)
 {
